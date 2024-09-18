e8dc00dc40183fd59baa2e11096710c779ad9424
AntoineLaurensUT3 <antoine.laurens12@gmail.com>
2024-09-18 09:48:03 +0200

    first commit

---
+++ ilu3-tp1/src/testsFonctionnels/TestJeuDeCartes.java
@@ -0,0 +1,23 @@
+package testsFonctionnels;
+import cartes.Attaque;
+import cartes.Borne;
+import cartes.Botte;
+import cartes.Carte;
+import cartes.Parade;
+import cartes.Type;
+
+public class TestJeuDeCartes {
+
+	
+	
+	
+	
+	public static void main() {
+		
+		
+		
+		
+	}
+	
+
+}
